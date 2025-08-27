88f7d57cd4bf81c603de62c4b1efc00855534c0f
kshi3430 <kshi3430@gmail.com>
2025-08-27 15:17:11 +0900

    kshi

---
+++ chap03/src/sec02/exam01/SingleOperatorExample.java
@@ -0,0 +1,23 @@
+package sec02.exam01;
+
+public class SingleOperatorExample {
+
+	public static void main(String[] args) {
+		int x=10;
+		int y=10;
+		int z;
+		
+		System.out.println("--------------");
+		x++;
+		y++;
+		System.out.println("x=" + x);
+		
+		System.out.println("--------------");
+		y--;
+		x--;
+		System.out.println("y=" + y);
+		// TODO Auto-generated method stub
+
+	}
+
+}
