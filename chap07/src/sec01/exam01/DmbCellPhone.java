e5ce23c78d00758dfd8c88934023b01cc9f20b92
kshi3430 <kshi3430@gmail.com>
2025-09-22 16:26:33 +0900

    kshi

--- chap07/src/sec01/exam01/DmbCellPhone.java
+++ chap07/src/sec01/exam01/DmbCellPhone.java
@@ -1,5 +1,24 @@
 package sec01.exam01;
 
-public class DmbCellPhone {
-
+public class DmbCellPhone extends CellPhone {
+	//필드
+	int channel;
+	
+	//생성자
+	DmbCellPhone(String model, String color, int channel){
+		this.model = model; //부모로부터 상속받은 필드
+		this.color = color;
+		this.channel = channel;
+	}
+	//메소드 
+	void turnOnDmb() {
+		System.out.println("채널 " + channel + "DMB 방송 수신을 시작합니다.");
+	}
+	void changeChannelDmb(int channel) {
+		this.channel = channel;
+		System.out.println("채널 " + channel + "번으로 바꿉니다.");
+	}
+	void turnOffDmb() {
+		System.out.println("DMB 방송 수신을 멈춥니다.");
+	}
 }
