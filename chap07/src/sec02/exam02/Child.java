75e1ccfdb6c60ffa55f2e39d50fd8bc37644b4bf
kshi3430 <kshi3430@gmail.com>
2025-10-13 13:45:06 +0900

    kshi

--- chap07/src/sec02/exam02/Child.java
+++ chap07/src/sec02/exam02/Child.java
@@ -1,5 +1,11 @@
 package sec02.exam02;
 
-public class Child {
-
+public class Child extends Parent {
+@Override
+	public void method2() {
+		System.out.println("Child-method2()");
+	}
+	public void method3() {
+		System.out.println("Child-method3()");
+	}
 }
---
+++ cote/src/cote/poly/Dog.java
@@ -0,0 +1,5 @@
+package cote.poly;
+
+public class Dog {
+
+}
